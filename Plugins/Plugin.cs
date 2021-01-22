b96d81d3ea97c42836017494ce1958f3c66804c5
Daniel Lozano Navas <d.lozanonavas@outlook.com>
2021-01-22 00:52:46 -0500

    Create: Plugin Type as Module derivate

---
+++ Plugins/Plugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FenixAlliance.ACL.Configuration.Interfaces;
+using FenixAlliance.ACL.Configuration.Types.ABP.Modular;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FenixAlliance.ABP.Hub.Plugins
+{
+    public class Plugin : Module
+    {
+        public override int ConfigureServices(IServiceCollection services, IConfiguration Configuration, IHostEnvironment Environment, ISuiteOptions Options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Configure(IApplicationBuilder app, IConfiguration Configuration, IHostEnvironment Environment, ISuiteOptions Options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
