f8ad002b83db90ab55fb09ead1f774a8e8e51333
Daniel Lozano Navas <d.lozanonavas@outlook.com>
2021-01-22 00:52:15 -0500

    Rename: Package Specification

--- Plugins/Specifications/Package.cs
+++ Plugins/Specifications/Package.cs
@@ -1,13 +1,13 @@
 ﻿using System.Xml.Serialization;
 
-namespace FenixAlliance.ABP.Hub.Plugins
+namespace FenixAlliance.ABP.Hub.Plugins.Packages
 {
     [XmlRoot(Namespace = "http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd", ElementName = "package")]
     public class Package
     {
         [XmlElement(Namespace = "http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd", ElementName = "metadata")]
-        public Metadata Metadata { get; set; }
+        public Metadata.Metadata Metadata { get; set; }
         [XmlElement(Namespace = "http://schemas.microsoft.com/packaging/2013/05/nuspec.xsd", ElementName = "files")]
-        public Files Files { get; set; }
+        public Files.Files Files { get; set; }
     }
 }
